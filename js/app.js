cda65e700cbda8c18d9b316c73e416963f727a23
Mark Goho <markgoho@gmail.com>
2016-10-30 10:47:13 -0400

    Updates to app

---
+++ js/app.js
@@ -0,0 +1,49 @@
+var $getMovies = $('#get-movies');
+var $movieResults = $('#movie-results');
+var $lightbox = $('.lightbox');
+var apiCall = "http://www.omdbapi.com/?s=star+wars&y=&plot=short&r=json";
+var movies = [];
+
+$getMovies.click(function () {
+  $.getJSON(apiCall, function(data) {
+    console.log(data.Search);
+    movies = data.Search;
+    $.each(movies, function (key, val) {
+      $movieResults.append('<div id=' + key + '><img src="' + val.Poster + '" alt="' + val.Title + ' poster"></div>');
+    }); //each
+  }); //getJSON
+  $getMovies.hide();
+}); //button click
+
+$('#movie-results').on('click', '> div', function () {
+  var index = parseInt($(this).attr('id'));
+  var imdb = movies[index].imdbID;
+  console.log(imdb);
+  var descriptionCall = "http://www.omdbapi.com/?i=" + imdb + "&plot=short&r=json";
+  console.log(descriptionCall);
+
+  $.getJSON(descriptionCall, function(data) {
+
+    console.log(data);
+
+    $lightbox.append(
+          '<div class="lightbox-result">' + 
+            '<img src="' + movies[index].Poster + '" alt="' + movies[index].Title + ' poster">' +
+            '<div class="movie-details">' +
+              '<h2>' + movies[index].Title + '</h2>' +
+              '<div>Released in: ' + movies[index].Year + '</div>' +
+              '<div>IMDB: <a href="http://www.imdb.com/title/' + movies[index].imdbID + '">' + movies[index].imdbID + '</a></div>' +
+              '<div class="description">' + data.Plot + '</div>' +
+            '</div>' +
+          '</div>'
+    );
+  });
+  
+
+  $lightbox.css('display', 'flex');
+});
+
+$lightbox.not($('.lightbox-result')).click(function() {
+  $lightbox.hide();
+  $lightbox.html("");
+});
